6feed347616b5de8118c06d902a0f9547e75ea33
Sarah <sarahpham99@hotmail.com>
2019-12-26 12:13:49 -0600

    Almost done with navigation bar

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -6,15 +6,15 @@ import DrawerToggleButton from '../SideDrawer/DrawerToggleButton';
 const toolbar = props => (
     <header className="toolbar">
         <nav className="toolbar_navigation">
-            <div>
-                <DrawerToggleButton />
+            <div className='toolbar_toggle-button'>
+                <DrawerToggleButton click={props.drawerClickHandler}/>
             </div>
             <div className="toolbar_logo"><a href="/"> THE LOGO</a></div>
             <div className="spacer"></div>
             <div className="toolbar_navigation-items">
                 <ul>
                     <li><a href="/">Products</a></li>
-                    <li><a href="/">Products</a></li>
+                    <li><a href="/">Users</a></li>
                 </ul>
             </div>
         </nav>
