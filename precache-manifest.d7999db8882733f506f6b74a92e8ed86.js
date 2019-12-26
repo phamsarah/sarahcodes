7d67535a729105123aa57281eadade69ad3e6d50
Sarah <sarahpham99@hotmail.com>
2019-12-26 08:56:37 -0600

    Updates

---
+++ precache-manifest.d7999db8882733f506f6b74a92e8ed86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1201a294fe33c3faf94956af012a0df5",
+    "url": "/sarahcodes/index.html"
+  },
+  {
+    "revision": "1a5d054e8b9291b19f2e",
+    "url": "/sarahcodes/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "85b27f39f19642abc987",
+    "url": "/sarahcodes/static/js/2.b8ed6a7d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/sarahcodes/static/js/2.b8ed6a7d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1a5d054e8b9291b19f2e",
+    "url": "/sarahcodes/static/js/main.a74f3f19.chunk.js"
+  },
+  {
+    "revision": "859ac9eba3f5ea352bf7",
+    "url": "/sarahcodes/static/js/runtime-main.962a78cb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sarahcodes/static/media/logo.5d5d9eef.svg"
+  }
+]);
